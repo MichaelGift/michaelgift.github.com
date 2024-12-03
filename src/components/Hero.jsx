f94626a96ee8b0a259950436fc6c8bbb261ab5c0
Michael Gift <simiyugift@outlook.com>
2024-12-03 04:30:04 +0300

    Update Name on Portfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,12 +37,12 @@ const Hero = () => {
           <div>
             <h1
               className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
-              Hi, I'm{' '}
+              Michael{' '}
               <span
                 className="sm:text-battleGray sm:text-[90px] 
                 text-eerieBlack text-[50px] font-mova
                 font-extrabold uppercase">
-                Michael
+                Gift
               </span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
